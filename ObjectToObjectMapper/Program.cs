92a78b044061522eb170ae3e09b3190f467a5793
Gunnar <gpeipman@hotmail.com>
2019-09-07 10:34:37 +0300

    empty project

---
+++ ObjectToObjectMapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectToObjectMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}   
